e502ceca31815daa33f8cc8ec4e27d005b20d954
Diddayngo <lamngo606@gmail.com>
2025-06-02 20:15:56 +0300

    Update: Completed removeElement

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -21,4 +21,17 @@ public class Problem1 {
         return k + 1;
     }
 
+    public int removeElement(int[] nums, int val) {
+
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i]; // Keep it
+                k++;
+            }
+        }
+        return k;
+
+    }
+
 }
