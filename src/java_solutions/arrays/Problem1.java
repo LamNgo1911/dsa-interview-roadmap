cc33b74d2e4f66762250a156c152995fb2270fd9
Diddayngo <lamngo606@gmail.com>
2025-06-02 16:30:52 +0300

    Update: Completed Remove Duplicates from Sorted Array

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -5,4 +5,20 @@ public class Problem1 {
     public static void main(String[] args) {
         System.out.println("✅ Java is running!");
     }
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+
+        int k = 0;
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[k] != nums[i]) {
+                ++k;
+                nums[k] = nums[i];
+            }
+        }
+
+        return k + 1;
+    }
+
 }
