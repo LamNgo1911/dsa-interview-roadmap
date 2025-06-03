95862c46e995fd1f9a6d60cb253986acf9e8a0ca
Diddayngo <lamngo606@gmail.com>
2025-06-03 12:13:26 +0300

    Update: Completed plusOne problem

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -34,4 +34,18 @@ public class Problem1 {
 
     }
 
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]+1<=9){
+                digits[i]+=1;
+                return digits;
+            }else{
+                digits[i]=0;
+            }
+       }
+       int[] ar=new int[digits.length+1];
+       ar[0]=1;
+       return ar;
+    }
+
 }
