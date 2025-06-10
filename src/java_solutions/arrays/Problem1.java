5241688f7ffcf4ca69e8c80191abea8b3acab616
Diddayngo <lamngo606@gmail.com>
2025-06-10 23:23:16 +0300

    Update: Solved Intersection of Two Linked Lists

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -180,4 +180,19 @@ public class Problem1 {
 
         return false;
     }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+
+        ListNode pA = headA;
+        ListNode pB = headB;
+
+        while (pA != pB) {
+            pA = (pA == null) ? headB : pA.next;
+            pB = (pB == null) ? headA : pB.next;
+        }
+
+        return pA;
+    }
 }
