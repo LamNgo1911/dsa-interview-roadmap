892ee827a30ddf6bef23c0c8d09d79b300e441aa
liam <lamngo606@gmail.com>
2025-07-01 13:19:27 +0300

    Update: Completed preorderTraversal

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -454,5 +454,23 @@ public class Problem1 {
         inorder(node.left, result);
         result.add(node.val);
         inorder(node.right, result);
+    }
 
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        preorder(root, result);
+
+        return result;
+    }
+
+    private void preorder(TreeNode node, List<Integer> result){
+        if(node == null){
+            return;
+        }
+
+        result.add(node.val);
+        preorder(node.left, result);
+        preorder(node.right, result);
+    }
 }
