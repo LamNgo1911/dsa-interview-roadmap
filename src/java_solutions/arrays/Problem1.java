283badbec92b84d54daed4dcb8f3cca07a7d8e32
liam <lamngo606@gmail.com>
2025-07-14 15:05:14 +0300

    Completed Two Sum IV - Input is a BST

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -633,4 +633,19 @@ class MyQueue {
     }
 }
 
+public boolean findTarget(TreeNode root, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        return dfs(root, k, set);
+    }
+
+     private boolean dfs(TreeNode node, int k, HashSet<Integer> set) {
+        if (node == null) return false;
+        
+        if (set.contains(k - node.val)) return true;
+        
+        set.add(node.val);
+        
+        return dfs(node.left, k, set) || dfs(node.right, k, set);
+    }
+
 }
