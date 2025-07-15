f29a7155b1cad2475407dc3a2b93c1a5dadd6ac2
liam <lamngo606@gmail.com>
2025-07-15 13:57:37 +0300

    Completed Count Complete Tree Nodes

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -710,4 +710,11 @@ class MyQueue {
         return left -1;
     }
 
+     public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
 }
