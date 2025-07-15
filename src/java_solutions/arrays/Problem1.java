ce61f2140e04cc44da8e26100687023326624e28
liam <lamngo606@gmail.com>
2025-07-15 13:00:15 +0300

    Completed mySqrt

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -687,4 +687,27 @@ class MyQueue {
         return left;
     }
 
+    public int mySqrt(int x) {
+        if(x == 0) return 0;
+        if(x == 1) return 1;
+
+        int left = 0;
+        int right = x;
+        
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            long square = (long) mid * mid;
+            if(square == x){
+                return mid;
+            } else if(square < x){
+                left = mid + 1;
+            } else{
+                right = mid - 1;
+            }
+        }
+
+        return left -1;
+    }
+
 }
