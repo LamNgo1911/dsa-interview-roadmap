cf08fd851fd9943278fa8a98c5c0300ba89c19ea
liam <lamngo606@gmail.com>
2025-07-22 23:10:52 +0300

    Completed First Bad Version

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -735,4 +735,21 @@ class MyQueue {
         return xorResult;
     }
 
+    public int firstBadVersion(int n) {
+
+        int left = 1;
+        int right = n;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(isBadVersion(mid)){
+                right = mid - 1;
+            } else{
+                left = mid + 1;
+            }
+        }
+        return left;
+        
+    }
+
 }
