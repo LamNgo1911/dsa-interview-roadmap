1b3b39dcc88f654d89d98f15e4f85f9b2bf8a33f
liam <lamngo606@gmail.com>
2025-07-24 23:12:20 +0300

    Completed intersection

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -752,4 +752,29 @@ class MyQueue {
         
     }
 
+      public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+
+        for(int i = 0; i < nums1.length; i++){
+            set1.add(nums1[i]);
+        }
+
+         Set<Integer> set2 = new HashSet<>();
+
+        for(int i = 0; i < nums2.length; i++){
+            if(set1.contains(nums2[i])){
+                set2.add(nums2[i]);
+            }
+        }
+
+        int[] result = new int[set2.size()];
+
+         int index = 0;
+        for (int num : set2) {
+            result[index++] = num;
+        }
+
+        return result;
+    }
+
 }
