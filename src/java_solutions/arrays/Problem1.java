59994a98ca15dcfd7c42a819b098d5521ad2d054
liam <lamngo606@gmail.com>
2025-07-31 14:41:28 +0300

    Completed Valid Perfect Square

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -801,4 +801,31 @@ class MyQueue {
         return arr;
     }
 
+    public boolean isPerfectSquare(int num) {
+
+        long left = 1;
+        long right = num/2;
+
+        if(num == 1){
+            return true;
+        }
+
+        while(left <= right){
+            long mid = left + (right - left)/2;
+
+            if(mid * mid == num) return true;
+
+            if(mid * mid > num){
+                right = mid - 1;
+            }
+
+            if(mid * mid < num){
+                left = mid + 1;
+            }
+        }
+
+        return false;
+
+    }
+
 }
