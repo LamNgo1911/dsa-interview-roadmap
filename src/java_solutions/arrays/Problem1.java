1c798bc7cd91591c844e1cdaac1903174c479063
liam <lamngo606@gmail.com>
2025-08-20 14:42:22 +0300

    Completed arrangeCoins

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -852,4 +852,24 @@ class MyQueue {
         }
     }
 
+    public int arrangeCoins(int n) {
+        int left = 0;
+        int right = n;
+
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            long rows = (long) mid * (mid + 1) / 2; // ✅ cast to long before multiply
+
+            if(rows == n){
+                return mid;
+            } else if(rows < n){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return right;
+    }
+
 }
