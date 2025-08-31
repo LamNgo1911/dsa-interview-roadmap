6add6ddf67c8eb599bdae6a7e06b1be1e68197aa
liam <lamngo606@gmail.com>
2025-08-31 23:23:24 +0300

    completed rotatedSearch

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -893,4 +893,31 @@ class MyQueue {
         
     }
 
+     public int rotatedSearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length -1;
+
+        while(left <= right){
+            int mid = left + (right-left)/2;
+
+            if(nums[mid] == target) return mid;
+
+            if(nums[left] <= nums[mid]){
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1; // target in left half
+                } else {
+                    left = mid + 1; // target in right half
+                }
+            } else{
+                 if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1; // target in right half
+                } else {
+                    right = mid - 1; // target in left half
+                }
+            }
+        }
+
+          return -1;
+    }
+
 }
