10fd5471ca33e4947e09436e219c511dd2cdaf10
liam <lamngo606@gmail.com>
2025-09-10 23:42:35 +0300

    Completed reverseList

--- src/java_solutions/arrays/Problem1.java
+++ src/java_solutions/arrays/Problem1.java
@@ -1012,4 +1012,16 @@ class MyQueue {
 
     }
 
+    public ListNode reverseList(ListNode head) {
+         if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
 }
